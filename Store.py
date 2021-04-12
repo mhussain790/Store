57d981130396f52814ca946b3c33edfc18e31563
Masud Hussain <mhussain790@gmail.com>
2021-04-11 21:29:16 -0700

    Second commit of the completed Store.py file including the main method so the program can be run as a script.

--- Store.py
+++ Store.py
@@ -26,22 +26,6 @@ class Product:
     def get_quantity_available(self):
         return self._quantity_available
 
-    # SET METHODS
-    def set_product_id(self, new_id):
-        self._product_id = new_id
-
-    def set_title(self, new_title):
-        self._title = new_title
-
-    def set_description(self, new_description):
-        self._description = new_description
-
-    def set_price(self, new_price):
-        self._price = new_price
-
-    def set_quantity_available(self, new_quantity_avail):
-        self._quantity_available = new_quantity_avail
-
     # Decrease quantity available by one
     def decrease_quantity(self):
         if self._quantity_available > 0:
@@ -54,12 +38,12 @@ class Customer:
         self._name = name
         self._customer_id = customer_id
         self._premium_member = premium_member
-        self.customer_cart = []
-
-    # initialize an empty list for hold product ID codes for customer cart
-    # customer_cart = []
+        self._customer_cart = []
 
     # A private collection (list, dictionary, etc) of Product ID codes with a get method
+    def get_customer_cart(self):
+        return self._customer_cart
+
     def get_name(self):
         return self._name
 
@@ -75,11 +59,11 @@ class Customer:
 
     # add product ID code to customer's cart
     def add_product_to_cart(self, new_product):
-        self.customer_cart.append(new_product)
+        self._customer_cart.append(new_product)
 
     # empty customer cart
     def empty_cart(self):
-        self.customer_cart.clear()
+        self._customer_cart.clear()
 
 
 class InvalidCheckoutError(Exception):
@@ -98,46 +82,23 @@ class Store:
     def add_member(self, new_customer):
         self._membership.append(new_customer)
 
-    # def lookup_product_from_id(self, product_id):
-    #     for item in self.inventory:
-    #         if item.get_product_id() == product_id:
-    #             return product_id
-    #         else:
-    #             return None
-
-    # TODO: RETURN PRODUCT
     def lookup_product_from_id(self, product_id):
-        for item in range(len(self._inventory)):
-            if self._inventory[item].get_product_id() == product_id:
-                return self._inventory[item]
-            else:
+        found = False
+        for products in self._inventory:
+            if product_id == products.get_product_id():
+                return products.get_title()
+                found = True
+            if not found:
                 return None
 
-    # TODO: RETURN MEMBER
     def lookup_member_from_id(self, customer_id):
+        found = False
         for member in self._membership:
             if customer_id == member.get_customer_id():
-                return str(member.get_name())
-            else:
+                return member.get_name()
+                found = True
+            elif not found:
                 return None
-
-    #
-    # def product_search(self, search_string):
-    #     # Initialize empty list to store case insensitive products
-    #     sorted_list = []
-    #
-    #     # Sort through inventory to find the matching product
-    #     for item in range(len(self.inventory)):
-    #         for word in self.inventory[item].get_title().split():
-    #             if search_string.lower() == word.lower():
-    #                 sorted_list.append(word)
-    #         break
-    #
-    #     # Sort unordered list
-    #     sorted_list.sort()
-    #
-    #     # Return empty sorted list
-    #     return sorted_list
 
     def product_search(self, search_string):
         # Initialize empty list to store case insensitive products
@@ -207,30 +168,24 @@ class Store:
 
 
 # TODO: MAIN METHOD
+def main():
+    myStore = Store()
+    prod = Product(1001, "mango", "i like mangos", 35, 10)
+    prod_2 = Product(1002, "orange", "juicy orange", 45, 2)
+    prod_3 = Product(1003, "apple", "green apple", 55, 1)
+    prod_4 = Product(1004, "MANGO", "large mango", 65, 3)
+    cust = Customer("test", 3534, False)
+
+    myStore.add_product(prod)
+    myStore.add_product(prod_2)
+    myStore.add_product(prod_3)
+    myStore.add_product(prod_4)
+    myStore.add_member(cust)
+
+    return myStore.check_out_member(3534)
+
+
+if __name__ == '__main__':
+    main()
+
 # TODO: UNIT TEST
-
-prod = Product(1001, "mango", "i like mangos", 35, 0)
-prod_2 = Product(1002, "orange", "juicy orange", 45, 300)
-prod_3 = Product(1003, "apple", "green apple", 55, 400)
-prod_4 = Product(1004, "MANGO", "large mango", 65, 100)
-cust = Customer("dani", 3534, False)
-# testing_inv = [1, 2, 3, 4, 5, 1001]
-# member_list = [444, 2494,3092392, 3534]
-# new = Store(prod,cust)
-# print(new.inventory)
-
-myStore = Store()
-myStore.add_product(prod)
-myStore.add_product(prod_2)
-myStore.add_product(prod_3)
-myStore.add_product(prod_4)
-myStore.add_member(cust)
-# print(myStore.inventory[0].get_product_id())
-# print(myStore.membership[0].get_customer_id())
-print(myStore.lookup_product_from_id(1001))
-print(myStore.lookup_member_from_id(3534))
-# print(myStore.product_search("mango"))
-#
-# print(myStore.add_product_to_member_cart(1003, 3534))
-
-print(myStore.check_out_member(354))
